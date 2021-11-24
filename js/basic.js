71115615f0b9a0fe0a8b4255ee22d32f521f8bca
Ahmed Teleb <a58846280@gmail.com>
2021-11-24 04:05:18 +0200

    Added missed files

---
+++ js/basic.js
@@ -0,0 +1,9 @@
+var logo = document.getElementsByClassName("logo")
+
+window.onload = function() {
+    document.body.className += " loaded";
+}
+
+function animateLogo(logo){
+    logo.style = '';
+}
